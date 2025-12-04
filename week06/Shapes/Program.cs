6194026a1b0cd300f78198d9778df05c618af96e
Davin Quist <davinjquist@gmail.com>
2025-12-04 16:17:56 -0700

    main

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,23 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        // Create a list of shapes
+        List<Shape> shapes = new List<Shape>();
+
+        // Add different shape types
+        shapes.Add(new Square("Red", 4));
+        shapes.Add(new Rectangle("Blue", 5, 3));
+        shapes.Add(new Circle("Green", 2.5));
+
+        // Display areas using polymorphism
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape Color: {shape.GetColor()}");
+            Console.WriteLine($"Area: {shape.GetArea():0.00}\n");
+        }
     }
 }
